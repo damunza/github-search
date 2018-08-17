b03b73d03c7f4948a63efe3a3881cb861d1287bf
damunza <danidamunza@gmail.com>
2018-08-17 17:36:21 +0300

    added a new function tafuta and updateprofile

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -10,20 +10,37 @@ export class GitComponent implements OnInit {
 
 user: any =[];
 works: any =[];
+public results;
+public majibu;
+searchItem:string ;
 
   constructor(private gitrequest:GithubRequestService) {
+
+   }
+
+tafuta(searchItem: HTMLInputElement){
+  this.gitrequest.updateProfile('cherucole').subscribe(data => {
+    this.results = data;
+    console.log (this.results)
+  });
+  // this.gitrequest.updateRepo('cherucole').subscribe(cow =>{
+  //   this.majibu=cow;
+  //   console.log(this.majibu)
+  // })
+
+    // was inside the constructor
+
     this.gitrequest.getprofile().subscribe(rep=>{
-      console.log(rep)
+      // console.log(rep)
       this.user=rep;
 
     });
 
     this.gitrequest.getrepos().subscribe(cat=>{
-      console.log(cat)
+      // console.log(cat)
       this.works=cat;
     })
-   }
-
+}
 
 
   ngOnInit() {
