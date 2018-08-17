232b9c11313c9e3bc11a93a3fa420b5a5caf649f
damunza <danidamunza@gmail.com>
2018-08-17 13:43:51 +0300

    modified the jason files

---
+++ src/app/git/git.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {GithubRequestService} from '../github-http/github-request.service';
+
+@Component({
+  selector: 'app-git',
+  templateUrl: './git.component.html',
+  styleUrls: ['./git.component.css']
+})
+export class GitComponent implements OnInit {
+
+user: any =[];
+works: any =[];
+
+  constructor(private gitrequest:GithubRequestService) {
+    this.gitrequest.getprofile().subscribe(rep=>{
+      console.log(rep)
+      this.user=rep;
+
+    });
+
+    // this.gitrequest.getrepos().subscribe(cat=>{
+    //   console.log(cat)
+    //   this.works=cat;
+    // })
+   }
+
+
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/github-http/github-request.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubRequestService {
+
+private username: string;
+
+  constructor(private http:HttpClient) {
+
+    this.username= 'damunza'
+
+  }
+
+getprofile(){
+  // return this.http.get('https://api.github.com/users/damunza?access_token=4688146874771f2ec799329662a4432e2b280111')
+
+  // return this.http.get(environment.apiUrl)
+
+  return this.http.get(environment.mzizi + this.username + '?access_token=' + environment.token)
+}
+// getrepos(){
+//     return this.http.get(environment.mzizi + this.username + '/repos?access_token=' + environment.token)
+// }
+
+}
