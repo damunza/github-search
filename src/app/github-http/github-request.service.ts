86ebf4dddd2c7855e4cd4031628d1d1d5c44de2f
damunza <danidamunza@gmail.com>
2018-08-18 14:12:27 +0300

    editted the update profile function

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -29,9 +29,9 @@ getrepos(){
 }
 
 updateProfile(searchItem:string){
-  let search_url = `${environment.mzizi}${searchItem}?access_token=${environment.token}`
-  return this.http.get(search_url);
-  // this.username=searchItem
+  // let search_url = `${environment.mzizi}${searchItem}?access_token=${environment.token}`
+  // return this.http.get(search_url);
+  this.username=searchItem
 }
 updateRepo(searchItem:string){
   let api_url=`${environment.mzizi}${searchItem}/repos?access_token=${environment.token}`
