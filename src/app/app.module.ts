50b62062f5db22ee347cd38061d9d434fc2a78dc
damunza <danidamunza@gmail.com>
2018-08-18 14:07:52 +0300

    imported the FormsModule from angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GitComponent } from './git/git.component';
@@ -13,7 +14,8 @@ import {GithubRequestService} from './github-http/github-request.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [GithubRequestService],
   bootstrap: [AppComponent]
